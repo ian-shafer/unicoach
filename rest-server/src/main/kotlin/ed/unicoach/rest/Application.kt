02e015879cf07e5141ffb18b80579277c8139d21
Ian Shafer <ian@1321.org>
2026-04-03 21:57:05 -0700

    02-hello-world-open-api-spec:8

--- rest-server/src/main/kotlin/ed/unicoach/rest/Application.kt
+++ rest-server/src/main/kotlin/ed/unicoach/rest/Application.kt
@@ -3,7 +3,8 @@ package ed.unicoach.rest
 import io.ktor.server.application.Application
 
 fun main(args: Array<String>) {
-  io.ktor.server.netty.EngineMain.main(args)
+  io.ktor.server.netty.EngineMain
+    .main(args)
 }
 
 fun Application.module() {
--- rest-server/src/main/kotlin/ed/unicoach/rest/Routing.kt
+++ rest-server/src/main/kotlin/ed/unicoach/rest/Routing.kt
@@ -1,20 +1,39 @@
 package ed.unicoach.rest
 
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.withCharset
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.response.respondText
+import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+
+fun Route.rejectUnsupportedMethods(vararg methods: HttpMethod) {
+  handle {
+    val allowedString = methods.joinToString(", ") { it.value }
+    call.response.headers.append(HttpHeaders.Allow, allowedString)
+    call.respondText(
+      text = "Method Not Allowed. Expected: $allowedString",
+      status = HttpStatusCode.MethodNotAllowed,
+    )
+  }
+}
 
 fun Application.configureRouting() {
   routing {
-    get("/hello") {
-      call.respondText(
-        text = "Hello, Ian. I love you 😘",
-        contentType = ContentType.Text.Plain.withCharset(Charsets.UTF_8),
-      )
+    route("/hello") {
+      get {
+        call.respondText(
+          text = "Hello, Ian. I love you 😘",
+          contentType = ContentType.Text.Plain.withCharset(Charsets.UTF_8),
+        )
+      }
+      rejectUnsupportedMethods(HttpMethod.Get)
     }
   }
 }
