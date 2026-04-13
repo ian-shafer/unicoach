0a094de8a656adcfc4d753067a40477001b11c16
Ian Shafer <ian@1321.org>
2026-04-12 19:30:20 -0700

    [09-global-config:3] Extract Web Boundaries (rest-server)
    - Abstracted Application.kt server lifecycle correctly exposing startServer
    - Validated native JwtConfig abstractions tightly

--- rest-server/src/main/kotlin/ed/unicoach/rest/Application.kt
+++ rest-server/src/main/kotlin/ed/unicoach/rest/Application.kt
@@ -1,34 +1,74 @@
 package ed.unicoach.rest
 
 import ed.unicoach.auth.AuthService
+import ed.unicoach.common.config.AppConfig
 import ed.unicoach.db.Database
 import ed.unicoach.db.DatabaseConfig
+import ed.unicoach.rest.plugins.JwtConfig
 import ed.unicoach.rest.plugins.configureSerialization
 import ed.unicoach.rest.plugins.configureStatusPages
 import ed.unicoach.util.Argon2Hasher
 import ed.unicoach.util.JwtGenerator
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.engine.EmbeddedServer
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) {
-  io.ktor.server.netty.EngineMain
-    .main(args)
+fun startServer(wait: Boolean = true): EmbeddedServer<*, *> {
+  val config =
+    AppConfig
+      .load("common.conf", "service.conf", "rest-server.conf")
+      .getOrThrow()
+
+  val dbConfig =
+    DatabaseConfig
+      .from(config)
+      .getOrThrow()
+
+  val jwtConfig =
+    JwtConfig
+      .from(config)
+      .getOrThrow()
+
+  val database = Database(dbConfig)
+
+  val hostStr = config.getString("server.host")
+  val portInt = config.getInt("server.port")
+
+  val server =
+    embeddedServer(Netty, port = portInt, host = hostStr) {
+      val applicationConfig = environment.config as? MapApplicationConfig
+      applicationConfig?.apply {
+        put("jwt.secret", jwtConfig.secret.value)
+        put("jwt.issuer", jwtConfig.issuer)
+        put("jwt.audience", jwtConfig.audience)
+      }
+
+      environment.monitor.subscribe(ApplicationStopped) {
+        database.close()
+      }
+
+      appModule(database, jwtConfig)
+    }
+
+  server.start(wait = wait)
+  return server
 }
 
-fun Application.module() {
+fun main() {
+  startServer(wait = true)
+}
+
+fun Application.appModule(
+  database: Database,
+  jwtConfig: JwtConfig,
+) {
   configureSerialization()
   configureStatusPages()
 
-  val dbConfig =
-    DatabaseConfig(
-      url = environment.config.propertyOrNull("database.url")?.getString() ?: "jdbc:postgresql://localhost:5432/unicoach",
-      user = environment.config.propertyOrNull("database.user")?.getString() ?: "unicoach",
-    )
-  val database = Database(dbConfig)
-
-  val jwtSecret = environment.config.propertyOrNull("jwt.secret")?.getString() ?: "secret"
-  val jwtIssuer = environment.config.propertyOrNull("jwt.issuer")?.getString() ?: "issuer"
-  val jwtGenerator = JwtGenerator(jwtSecret, jwtIssuer)
-
+  val jwtGenerator = JwtGenerator(jwtConfig.secret.value, jwtConfig.issuer)
   val argon2Hasher = Argon2Hasher()
   val authService = AuthService(database, jwtGenerator, argon2Hasher)
 
